f1b3f1888baf06c391be48fc8d9d63d1d0f51214
flareair <flareair0@gmail.com>
2016-05-24 16:59:31 +0300

    First alpha works

--- client/pages/controllers/BoardCtrl.js
+++ client/pages/controllers/BoardCtrl.js
@@ -1,11 +1,25 @@
 'use strict';
 
 export default class BoardCtrl {
-    constructor(metaDataService, menuService) {
+    constructor(metaDataService, menuService, boardService) {
         this.title = 'BoardOfShame';
         metaDataService.setPageTitle(this.title);
         menuService.setActiveItem('/');
+
+        this.boardService = boardService;
+
+        this.scammers = [];
+
+        this.activate();
+    }
+
+    activate() {
+        this.boardService.getAllScammers()
+            .then((res) => {
+                console.log(res.data[0]);
+                this.scammers = res.data;
+            });
     }
 }
 
-BoardCtrl.$inject = ['metaDataService', 'menuService'];
+BoardCtrl.$inject = ['metaDataService', 'menuService', 'boardService'];
