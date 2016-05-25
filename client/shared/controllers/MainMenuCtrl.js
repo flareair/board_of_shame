f820abe30db625421ac101eaafb2a23d214e38ea
flareair <flareair0@gmail.com>
2016-05-25 14:47:28 +0300

    Mobile collapse menu now works

--- client/shared/controllers/MainMenuCtrl.js
+++ client/shared/controllers/MainMenuCtrl.js
@@ -4,11 +4,16 @@ export default class MainMenuCtrl {
     constructor(menuService) {
         this.menuService = menuService;
         this.menuItems = menuService.menuItems;
+        this.isCollapsed = false;
     }
 
     isActive(url) {
         // need to improve
         return this.menuService.getActiveItem() === url ? 'active' : '';
+    }
+
+    toggle() {
+        this.isCollapsed = !this.isCollapsed;
     }
 }
 
