2d9284bb2b5c5bf64481b2b21fa3aa4a2a4a7dd0
flareair <flareair0@gmail.com>
2016-05-30 12:23:27 +0300

    Added angularlitics

--- client/app.js
+++ client/app.js
@@ -5,6 +5,9 @@ import './main.less';
 import angular from 'angular';
 import ngRouter from 'angular-route';
 import ngSanitize from 'angular-sanitize';
+import angulartics from 'angulartics';
+import ga from 'angulartics-google-analytics';
+
 
 import shared from './shared/sharedModule';
 import pages from './pages/pagesModule';
@@ -14,6 +17,8 @@ import board from './board/boardModule';
 export default angular.module('app', [
     ngRouter,
     ngSanitize,
+    angulartics,
+    ga,
     shared,
     pages,
     board
