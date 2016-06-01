cbdeee99217ab8e45ad3de8dcb6026e430530511
flareair <flareair0@gmail.com>
2016-06-01 12:00:22 +0300

    Added filter for names

--- client/board/boardModule.js
+++ client/board/boardModule.js
@@ -7,13 +7,18 @@ import boardRouter from './boardRouter';
 
 
 import boardService from './services/boardService';
+
 import linkFilter from './filters/linkFilter';
+import nameFilter from './filters/nameFilter';
+
+console.log(nameFilter);
 
 import BoardCtrl from './controllers/BoardCtrl';
 
 export default angular.module('app.board', [ngRouter])
     .config(boardRouter)
     .filter('linkFilter', linkFilter)
+    .filter('nameFilter', nameFilter)
     .service('boardService', boardService)
     .controller('BoardCtrl' ,BoardCtrl)
     .name;
