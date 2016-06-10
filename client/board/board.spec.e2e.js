7ebb50cb0c6c1fc6f43a4ccdeff301b3c5c09144
flareair <flareair0@gmail.com>
2016-06-10 11:54:53 +0300

    Add board e2e test boilerplate

---
+++ client/board/board.spec.e2e.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+let expect = chai.expect;
+
+describe('Board page (desktop)', () => {
+
+    before(() => {
+        browser.driver.manage().window().setSize(1280, 768);
+    });
+
+    beforeEach(() => {
+        browser.get('/');
+    });
+
+    describe('static content', () => {
+        it('should have right page title', () => {
+            expect(browser.getTitle()).to.eventually.equal('BoardOfShame Список мошенников');
+        });
+
+        it('should have right h1 caption', () => {
+            expect(element(by.css('.board-caption')).getText()).to.eventually.equal('Поиск');
+        });
+
+        it('should have right active menu item', () => {
+            expect(element(by.css('.navbar .active')).getText()).to.eventually.equal('Список');
+        });
+    });
+
+
+    describe('board body', () => {
+        it('should contain a lot of board items', () => {
+            expect(element.all(by.css('.board-item')).count()).to.eventually.be.above(50);
+        });
+    });
+
+});
