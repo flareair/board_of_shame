58731a95a1a21bbaa5daa166f5af22e58b6f9b0b
flareair <flareair0@gmail.com>
2016-06-16 15:26:37 +0300

    Added special test Google table

---
+++ server/config/sheet.js
@@ -0,0 +1,5 @@
+module.exports = {
+    sheetId: process.env.TABLE === 'test' ? '1JXhnI4v8aztxP2Y22r3IFIl536_nuewqqutRKQXbGeY' : '12k92qZdkAvnff38qMY8P0U8F1UVWjE0sxhEnEGDfLd8',
+    begin: 'B9',
+    end: 'E5000',
+};
