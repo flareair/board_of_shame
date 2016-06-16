e0e5ff81dc4f23a6bf856b9f44fa2b9615989300
flareair <flareair0@gmail.com>
2016-06-16 15:59:12 +0300

    Add test for board search filter

--- client/board/board.spec.e2e.js
+++ client/board/board.spec.e2e.js
@@ -8,6 +8,8 @@ let expect = chai.expect;
 
 describe('Board page (desktop)', () => {
 
+    let searchField = {};
+
     before(() => {
         browser.driver.manage().window().setSize(1280, 768);
     });
@@ -40,6 +42,37 @@ describe('Board page (desktop)', () => {
     describe('board footer info', () => {
         it('should contain right info about items', () => {
             expect(element(by.css('.board-footer')).getText()).to.eventually.equal('Всего найдено: 11');
+        });
+    });
+
+    describe('board search filter', () => {
+
+        beforeEach(() => {
+            searchField = element(by.css('.input-lg'));
+        });
+
+        it('should filter one scammer correctly', () => {
+            searchField.sendKeys('Михаил Кукурузов');
+
+            expect(element.all(by.css('.board-item')).count()).to.eventually.equal(1);
+            expect(element.all(by.css('.board-item')).get(0).getText()).to.eventually.contain('Михаил Кукурузов');
+            expect(element(by.css('.board-footer')).getText()).to.eventually.equal('Всего найдено: 1');
+        });
+
+        it('should filter group of scammers correctly', () => {
+            searchField.sendKeys('оракл');
+
+            expect(element.all(by.css('.board-item')).count()).to.eventually.equal(6);
+            expect(element.all(by.css('.board-item')).get(5).getText()).to.eventually.contain('Китайские подделки');
+            expect(element(by.css('.board-footer')).getText()).to.eventually.equal('Всего найдено: 6');
+        });
+
+        it('should display right message if nothing was found', () => {
+            searchField.sendKeys('zzzzblablaba');
+
+            expect(element.all(by.css('.board-item')).count()).to.eventually.equal(0);
+
+            expect(element(by.css('.board-footer')).getText()).to.eventually.equal('Ничего не найдено');
         });
     });
 
